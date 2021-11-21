dcd0b8da14e43d32e14003127f10229ef9fd9400
romanpastu <roman.pastushenko.1997@gmail.com>
2021-11-21 16:26:13 +0100

    go back button

--- issues-front/src/Components/Issue/index.tsx
+++ issues-front/src/Components/Issue/index.tsx
@@ -4,10 +4,15 @@ import { useLocation } from 'react-router-dom';
 
 // slug console-34595-146303
 
-const Issue = () => {
+const Issue = ({ history } : any) => {
   const location = useLocation();
   const filteredlocation = location.pathname.split('/')[2];
   const toDisplayEl = issuesJson.filter(i => i.slug === filteredlocation)[0];
+
+  const goBack = () => {
+    history.push('/home');
+  };
+
   return (
     <>
       <p>This is the issue {filteredlocation}</p>
@@ -15,6 +20,7 @@ const Issue = () => {
         <img className="article-img" src={toDisplayEl.cover_image} alt={toDisplayEl.name} />
         <p>Title: {toDisplayEl.name}</p>
         <p>Description: {toDisplayEl.description}</p>
+        <button onClick={goBack}>goBack</button>
       </div>
     </>
   );
