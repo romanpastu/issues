1853b17ad8ef8d38c4b98fa786ab652e05a3c8cb
romanpastu <roman.pastushenko.1997@gmail.com>
2021-11-21 20:32:09 +0100

    search by keywords

--- issues-front/src/Components/Containers/Issues/index.tsx
+++ issues-front/src/Components/Containers/Issues/index.tsx
@@ -16,6 +16,12 @@ function splitArray (flatArray: any, itemsPerPage: any) {
   return result;
 }
 
+function useInput ({ type }: any) {
+  const [value, setValue] = useState('');
+  const input = <input value={value} onChange={e => setValue(e.target.value)} type={type} />;
+  return [value, input];
+}
+
 const IssuesItems: React.FC<RouteComponentProps> = function ({ history }) {
   const [data, setData] = useState<any>([]);
   const [error, setError] = useState<any>(false);
@@ -25,15 +31,30 @@ const IssuesItems: React.FC<RouteComponentProps> = function ({ history }) {
   const [currentPage, setCurrentPage] = useState<any>(1);
   const [itemsPerPage] = useState<any>(13);
   const [numberOfPages, setNumberOfPages] = useState<any>(null);
+  const [searchValue, searchInput] = useInput({ type: 'text' });
+  const [keywords, setKeywords] = useState<any>([]);
 
-  const getIssues = () => {
-    axios.get(`${backendUrl}/issues`)
+  const getIssues = (keywords?: any) => {
+    setLoaded(false);
+    setFetched(false);
+    setError(false);
+    setNumberOfPages(null);
+    setData([]);
+    setPaginatedIssues([]);
+
+    let url = `${backendUrl}/issues`;
+    if (keywords) {
+      url += `?q=${keywords.toString()}`;
+    }
+    axios.get(url)
       .then(res => {
         if (res.status === 400) {
           setError(true);
+          setKeywords([]);
         } else if (res.status === 200) {
           setData((state: any) => [...state, res?.data]);
           setFetched(true);
+          setKeywords([]);
         }
       }).catch(() => {
         setError(true);
@@ -44,6 +65,17 @@ const IssuesItems: React.FC<RouteComponentProps> = function ({ history }) {
     // set the number of pages
     getIssues();
   }, []);
+
+  const search = () => {
+    const searchValues = searchValue.toString().split(',').map(s => s.trim());
+    setKeywords((oldArr: any) => [...oldArr, ...searchValues]);
+  };
+
+  useEffect(() => {
+    if (keywords.length > 0) {
+      getIssues(keywords);
+    }
+  }, [keywords]);
 
   useEffect(() => {
     if (fetched) {
@@ -59,13 +91,6 @@ const IssuesItems: React.FC<RouteComponentProps> = function ({ history }) {
       setLoaded(true);
     }
   }, [numberOfPages]);
-
-  useEffect(() => {
-    console.log('paginatedIssues');
-    console.log(paginatedIssues);
-    console.log(loaded);
-    console.log(error);
-  }, [paginatedIssues]);
 
   const firstPage = () => {
     setCurrentPage(1);
@@ -89,30 +114,31 @@ const IssuesItems: React.FC<RouteComponentProps> = function ({ history }) {
   };
 
   return (
-    <div> {
-      !error && loaded
+    <div>
+      {searchInput} <button onClick={search}>search</button>
+      {!error && loaded
         ? <>
-            <section className="cards">
-                {paginatedIssues?.length > 0 && paginatedIssues[currentPage - 1]?.length > 0 && paginatedIssues[currentPage - 1].map((i: any) => {
-                  return (
-                        <article key={i.slug} onClick={() => redirect(i.id)}>
-                            <img className="article-img" src={i.cover_image} alt={i.name} />
-                            <h1 className="article-title">
-                                {i.name}
-                            </h1>
-                        </article>
-                  );
-                })}
-            </section>
-            <div>
-                <button onClick={firstPage} disabled={currentPage === 1}>First page</button>
-                <button onClick={nextPage} disabled={currentPage === numberOfPages}>Next Page</button>
-                <button onClick={previousPage} disabled={currentPage === 1}>Previous Page</button>
-                <button onClick={lastPage} disabled={currentPage === numberOfPages}>Last Page</button>
-            </div>
-            </>
+          <section className="cards">
+            {paginatedIssues?.length > 0 && paginatedIssues[currentPage - 1]?.length > 0 && paginatedIssues[currentPage - 1].map((i: any) => {
+              return (
+                <article key={i.slug} onClick={() => redirect(i.id)}>
+                  <img className="article-img" src={i.cover_image} alt={i.name} />
+                  <h1 className="article-title">
+                    {i.name}
+                  </h1>
+                </article>
+              );
+            })}
+          </section>
+          <div>
+            <button onClick={firstPage} disabled={currentPage === 1}>First page</button>
+            <button onClick={nextPage} disabled={currentPage === numberOfPages}>Next Page</button>
+            <button onClick={previousPage} disabled={currentPage === 1}>Previous Page</button>
+            <button onClick={lastPage} disabled={currentPage === numberOfPages}>Last Page</button>
+          </div>
+        </>
         : <p>loading</p>
-    }
+      }
     </div>
 
   );
