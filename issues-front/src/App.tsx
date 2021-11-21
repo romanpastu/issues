3266f685aa6183d124fc0c7fea0391be9033385e
romanpastu <roman.pastushenko.1997@gmail.com>
2021-11-21 16:22:24 +0100

    router fix

--- issues-front/src/App.tsx
+++ issues-front/src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import IssuesContainer from './Components/Containers/Issues';
 import Issue from './Components/Issue';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 const App: React.FC = function () {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/home" component={IssuesContainer} exact />
-        {/* <Route path="/" render={() => <Redirect to="/home" />} /> */}
         <Route path="/issue/:id" component={Issue} exact/>
+        <Route path="/" render={() => <Redirect to="/home" />} />
       </Switch>
     </BrowserRouter>
   );
