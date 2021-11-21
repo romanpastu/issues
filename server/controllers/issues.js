427e88a043515b286e21a78580ffecec4da2b605
romanpastu <roman.pastushenko.1997@gmail.com>
2021-11-21 18:22:45 +0100

    backend controllers logic

--- server/controllers/issues.js
+++ server/controllers/issues.js
@@ -1,12 +1,32 @@
 
-import IssuesJson from "../issues.json"
+IssuesJson = require("../issues.json")
 
-const getAllIssues = () => {
-    console.log("getallissues")
+const getAllIssues = (req, res) => {
+    let arrayToCheck;
+    if (req.query.q) arrayToCheck = req.query.q.split(",");
+    const filteredJson = IssuesJson.map(({ id, name, cover_image, description }) => ({ id, name, cover_image, description }))
+    let filteredJsonKeyword;
+    if (arrayToCheck && arrayToCheck.length > 0) {
+        filteredJsonKeyword = filteredJson.filter(({ name, description }) =>
+            arrayToCheck.some(subStr => name.includes(subStr) || description.includes(subStr)))
+    }
+    if (filteredJsonKeyword) {
+        res.status(200).json(filteredJsonKeyword)
+    } else {
+        res.status(200).json(filteredJson)
+    }
 }
 
-const getIssue = (id) => {
-    console.log("getissue" + id || "")
+const getIssue = (req, res) => {
+    let id = req.params.id
+    console.log(req.params.id)
+    const filteredIssue = IssuesJson.filter(i => i.id == id)[0]
+    if (filteredIssue) {
+        
+        res.status(200).json({id: filteredIssue.id, name: filteredIssue.name ,cover_image: filteredIssue.cover_image, description: filteredIssue.description})
+    } else {
+        res.status(400).json({ error: "not found error" })
+    }
 }
 
 module.exports = {
